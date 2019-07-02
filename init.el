9ada4a463327df1d87b81f26b6fee72f9f2f775d
Artem Chernyak <artem.chernyak@ocelotconsulting.com>
2019-07-01 21:59:18 -0500

    updated fonts

--- init.el
+++ init.el
@@ -228,8 +228,8 @@ It should only modify the values of Spacemacs settings."
 
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Monoid"
-                               :size 13
+   dotspacemacs-default-font '("Victor Mono"
+                               :size 16
                                :weight normal
                                :width normal)
 
