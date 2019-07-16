a8a3c349399601a9b233397e839a4727170b0aad
Artem Chernyak <artem.chernyak@ocelotconsulting.com>
2019-07-16 10:34:51 -0500

    changed theme to gruvbox

---
+++ layers/vue/packages.el
@@ -0,0 +1,10 @@
+(setq vue-packages
+      '(vue-mode))
+
+(setq vue-excluded-packages '())
+
+(defun vue/init-vue-mode ()
+  (use-package vue-mode
+    :config
+    ;; 0, 1, or 2, representing (respectively) none, low, and high coloring
+    (setq mmm-submode-decoration-level 0)))
